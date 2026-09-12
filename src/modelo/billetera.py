debacfdd07d5bf623e06324ff89f379293239dcd
juandiegoudm <jgiraldo827@soyudemedellin.edu.co>
2026-09-12 17:59:50 -0500

    feat: Agregando nueva funcionalidad al codigo (RF2, RF3, RF6, RF9)

--- src/modelo/billetera.py
+++ src/modelo/billetera.py
@@ -1,4 +1,5 @@
 from modelo import Bolsillo, Tarjeta, CDT, Transaccion
+from datetime import date, datetime
 
 class Billetera:
     def __init__(self, disponible: float):
@@ -8,3 +9,23 @@ class Billetera:
         self.tarjetas: list[Tarjeta] = []
         self.cdts: list[CDT] = []
         self.historial: list[Transaccion] = []
+
+    def depositar_dinero(self, monto:float,) -> None:
+        if monto <=0:
+            raise ValueError("monto invalido, ingrese monto mayor que 0")
+        self.disponible +=monto
+        transaccion=Transaccion(monto,datetime.today().replace(microsecond=0), "depósito", "disponible")
+        self.historial.append(transaccion)
+
+    def retirar_dinero(self, monto: float) ->None:
+        if monto > self.disponible:
+            raise ValueError("monto invalido, dinero insuficiente en disponible")
+        self.disponible -=monto
+        transaccion=Transaccion(monto,datetime.today().replace(microsecond=0), "disponible", "fuera")
+        self.historial.append(transaccion)
+
+    def consultar_disponible(self) ->float:
+        return self.disponible
+
+    def consultar_historial_transacciones(self) -> list:
+        return [trans for trans in self.historial]
--- src/modelo/transaccion.py
+++ src/modelo/transaccion.py
@@ -1,9 +1,9 @@
 import datetime
 
 class Transaccion:
-    def __init__(self, monto:float, fecha: datetime, origen: str, destino:str, impuesto4x1000:float ):
+    def __init__(self, monto:float, fecha: datetime, origen: str, destino:str):
         self.monto = monto
         self.fecha = fecha
         self.origen = origen
         self.destino = destino
-        self.impuesto4x1000 = impuesto4x1000
+       

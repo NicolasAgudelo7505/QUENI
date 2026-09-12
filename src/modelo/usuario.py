dc02af1db83a5c64914cdc663f8940bdbce207ed
Nicolás A <nicolasagudelolopez@gmail.com>
2026-09-12 17:42:27 -0500

    feat: agregando métodos a la clase bolsillo, usuario y cdt

--- src/modelo/usuario.py
+++ src/modelo/usuario.py
@@ -1,7 +1,15 @@
-from billetera import Billetera 
+from billetera import Billetera
+
+
 class Usuario:
-    def __init__(self, nombre:str, documento:str, sueldo:float):
+    def __init__(self, nombre: str, documento: str, sueldo: float, billetera: Billetera):
         self.nombre = nombre
         self.documento = documento
         self.sueldo = sueldo
-        self.billetera : Billetera = None 
+        self.billetera: Billetera = billetera
+
+
+    def calcular_ahorro_mensual(self, bolsillo, fecha_actual=None):
+            monto_calculado = bolsillo.calcular_ahorro_mensual_sugerido(fecha_actual)
+            es_viable = monto_calculado <= self.sueldo
+            return {"monto_sugerido": monto_calculado, "viable": es_viable}
